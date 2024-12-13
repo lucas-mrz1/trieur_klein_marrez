592c0ef85d613c045fa90b2073fc2ac1d4347951
lucas-mrz1 <lucas.marrez@gmail.com>
2024-12-13 17:45:17 +0100

    programme avec capteur de couleur, ok

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 #include <ESP32Encoder.h>
 #include "rgb_lcd.h"
-#include <Adafruit_I2CDevice.h>
+#include "Adafruit_TCS34725.h"
 #include <SPI.h>
 #include <Wire.h>
 
 #define CLK 23 // CLK ENCODER 
 #define DT 19 // DT ENCODER 
 
-//Adafruit_TCS34725 tcs = Adafruit_TCS34725();
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
 ESP32Encoder encoder;
 rgb_lcd lcd;
 
@@ -50,6 +50,12 @@ void setup() {
   encoder.setCount ( 0 );
 
   //Init capteur couleur
+   if (tcs.begin()) {
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1);
+  }
 }
 
 void loop() 
@@ -82,5 +88,20 @@ ledcWrite(pwmChannel, vitesse);
 // encodeur
 long newPosition = encoder.getCount();
 e = newPosition;
+
+// capteur couleur
+uint16_t r, g, b, c, colorTemp, lux;
+tcs.getRawData(&r, &g, &b, &c);
+  // colorTemp = tcs.calculateColorTemperature(r, g, b);
+  colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
+  lux = tcs.calculateLux(r, g, b);
+
+  Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
+  Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
+  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+  Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
+  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
+  Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
+  Serial.println(" ");
 
 }
