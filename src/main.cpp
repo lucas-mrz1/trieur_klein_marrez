be775b05af03eddeac63ffbbacb480ac18fd2c8c
lucas-mrz1 <lucas.marrez@gmail.com>
2024-12-19 17:26:10 +0100

    programme final, complet

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS347
 ESP32Encoder encoder;
 rgb_lcd lcd;
 
-int i = 0;
+int i = 0, tour=0, bozpos = 0, csg=0;
 int v1, v2, vts;
 int BP1 = 0, BP2 = 12, BP3 = 2;
 int val1 = 0, val2 = 0, val3 = 0;
@@ -23,18 +23,28 @@ int vbp2;
 int phase = 26;
 int encodeurA = 23, encodeurB = 19, vitesse;
 
-int moteur = 27, pwmChannel1 = 0, pwmChannel2 = 1;
+int moteur = 27, pwmChannel1 = 0, pwmChannel2 = 2;
 
-int frequence = 25000;
-int frequence2 = 50;
-int resolution = 11;
-int resolution2 = 16;
+int frequence = 25000, frequence2 = 50;
+int resolution = 11, resolution2 = 16;
 int consigne;
 int servo  = 13;
+int posi = 0;
+char etat = 0;
 
-
-//accélérometre
-
+//position
+void position ( long pos)
+{
+  if((bozpos<((pos-1)*103+75))&&(bozpos>((pos-1)*103+65)))
+  csg =0;
+  else
+  {
+    if(bozpos<((pos-1)*103+65))
+    csg = 1;
+    if(bozpos>((pos-1)*103+75))
+    csg = -1;
+  }
+}
 
 // fonction asservissement
 void vTaskPeriodic(void *pvParameters)
@@ -49,42 +59,43 @@ void vTaskPeriodic(void *pvParameters)
   xLastWakeTime = xTaskGetTickCount();
   while (1)
   {
-    // // calcul vitesse rotation
-    // int newPosition = encoder.getCount();
-    // vts = oldPosition - newPosition;
-    // oldPosition = newPosition;
+    // calcul vitesse rotation
+    int newPosition = encoder.getCount();
+    posi = encoder.getCount();
+    vts = oldPosition - newPosition;
+    oldPosition = newPosition;
 
-    // //calcul de l'erreur+ erreur intégrale
-    // Erreurtot = Erreurtot + erreur;
-    // erreur = consigne - vts;
-    // vitesse = k * erreur + Ki * Erreurtot;
+    //calcul de l'erreur+ erreur intégrale
+    Erreurtot = Erreurtot + erreur;
+    erreur = consigne - vts;
+    vitesse = k * erreur + Ki * Erreurtot;
 
-    // //bride de l'erreur intégrale
-    // if(Erreurtot>30)
-    //   Erreurtot=Erreurtot-10;
+    //bride de l'erreur intégrale
+    if(Erreurtot>30)
+      Erreurtot=Erreurtot-10;
 
-    // //si vitesse augmente
-    // if (vitesse > 0)
-    // {
-    //   if (vitesse > 2047)
-    //     vitesse = 2047;
-    //   digitalWrite(phase, 1);
-    //   ledcWrite(pwmChannel1, vitesse);
-    // }
+    //si vitesse augmente
+    if (vitesse > 0)
+    {
+      if (vitesse > 2047)
+        vitesse = 2047;
+      digitalWrite(phase, 1);
+      ledcWrite(pwmChannel1, vitesse);
+    }
 
-    // //si vitesse diminue
-    // if (vitesse < 0)
-    // {
-    //   if (vitesse < -2047)
-    //     vitesse = -2047;
-    //   digitalWrite(phase, 0);
-    //   ledcWrite(pwmChannel1, -vitesse);
-    // }
+    //si vitesse diminue
+    if (vitesse < 0)
+    {
+      if (vitesse < -2047)
+        vitesse = -2047;
+      digitalWrite(phase, 0);
+      ledcWrite(pwmChannel1, -vitesse);
+    }
 
-    // // Endort la tâche pendant le temps restant par rapport au réveil,
-    // // ici 100ms, donc la tâche s'effectue ici toutes les 100ms.
-    // // xLastWakeTime sera mis à jour avec le nombre de ticks au prochain
-    // // réveil de la tâche.
+    // Endort la tâche pendant le temps restant par rapport au réveil,
+    // ici 100ms, donc la tâche s'effectue ici toutes les 100ms.
+    // xLastWakeTime sera mis à jour avec le nombre de ticks au prochain
+    // réveil de la tâche.
     vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(10));
   }
 }
@@ -132,24 +143,22 @@ void setup()
 
   // Création de la tâche périodique
   xTaskCreate(vTaskPeriodic, "vTaskPeriodic", 10000, NULL, 2, NULL);
-
-  //Servo
-
-  //Init acélérometre
-
 }
 
 void loop()
 {
+  //position
+  tour = encoder.getCount()/410;
+  bozpos= encoder.getCount()- 410 * tour;
   // Utilisation boutton + pot
   val1 = digitalRead(BP1);
   val2 = digitalRead(BP2);
   val3 = digitalRead(BP3);
-  consigne = analogRead(pot);
-  newpot = map (consigne, 0, 4095, 3850, 4915);
+  //consigne = analogRead(pot)/150;
+  //newpot = map (consigne, 0, 4095, 3000, 7000);
 
   lcd.setCursor(0, 0);
-  lcd.printf("newpot %3d           ", newpot);
+  lcd.printf("pot %3d           ", consigne);
 
   // capteur couleur
   uint16_t r, g, b, c, colorTemp, lux;
@@ -158,7 +167,7 @@ void loop()
   // colorTemp = tcs.calculateColorTemperature(r, g, b);
   colorTemp = tcs.calculateColorTemperature_dn40(r, g, b, c);
   lux = tcs.calculateLux(r, g, b);
-  /*
+  
     Serial.print("Color Temp: ");
     Serial.print(colorTemp, DEC);
     Serial.print(" K - ");
@@ -178,11 +187,37 @@ void loop()
     Serial.print(c, DEC);
     Serial.print(" ");
     Serial.println(" ");
-  */
  
  //servomoteur
+// if(val1 == LOW)
+//   {
+// ledcWrite(pwmChannel2, 5800);
+//   }
+// if(val3 == LOW)
+//   {
+// ledcWrite(pwmChannel2, 4769);
+//   }
+//   if(val2== LOW)
+//   {
+// ledcWrite(pwmChannel2, 3826);
+//   }
 
-ledcWrite(pwmChannel2, newpot);
- 
+//
+switch(etat)
+{
+case 0: 
+ledcWrite(pwmChannel2, 4769);
+  consigne = 1;
+  if (lux >= 200) etat = 2;
+  break;
 
+case 2:
+delay(1500);
+consigne = 0;
+delay(500);
+ledcWrite(pwmChannel2, 3826);
+delay (500);
+etat = 0;
+break;
+}
 }
